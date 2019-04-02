6eb6f4c93b5627adea43ae869a32a9f61487b0f6
Christoffer Sundqvist <fiskmoz96@gmail.com>
2019-04-02 13:24:53 +0200

    Class-based views with simple mixins

--- jobylon/views.py
+++ jobylon/views.py
@@ -2,25 +2,45 @@ from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
 
-# The view for the homepage.
-def homepage_view(request):
-    if not request.user.is_authenticated: 
-        return redirect('login')
-    users = User.objects.all()
-    return render(request, 'home.html', {'users': users})
+class UserObjectMixin(object):
+    
+    def get_all_users(self):
+        return User.objects.all()
 
-# The view for the registration form.
-def signup_view(request):
-    if request.method == 'POST':
+class HomepageView(UserObjectMixin,TemplateView):
+    template_name = 'home.html'
+    def get(self, request):
+        if not request.user.is_authenticated:
+            return redirect('login')
+        users = self.get_all_users()
+        context = {
+            'users': users
+        }
+        return render(request,self.template_name, context)
+
+class SignupView(TemplateView):
+
+    template_name = 'registration/signup.html'
+
+    def get(self, request):
+        form = UserCreationForm()
+        context = {
+            'form' : form
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request):
         form = UserCreationForm(request.POST or None)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
+            user = authenticate(user=username, password=raw_password)
             login(request, user)
             return redirect('home')
-    else:
-        form = UserCreationForm()
-    return render(request, 'registration/signup.html', {'form': form})
+        context = {
+            'form' : form,
+        }
+        return render(request, self.template_name, context)
--- messaging/views.py
+++ messaging/views.py
@@ -3,21 +3,52 @@ from .models import Message
 from django.db.models import Q
 from .forms import MessageForm
 from django.contrib.auth.models import User
+from django.views.generic import TemplateView
+
+# Defined a mixin for the TemplateView. This can be used to reduce redundancy by having this class
+# define and be called upon when accessing Message objects.
+
+class MessageObjectMixin(object):
+    
+    def filter_messages(self, sender, receiver):
+        return Message.objects.filter(Q(Sender=sender, Receiver= receiver) | Q(Sender=receiver, Receiver = sender)).order_by('Date')
 
 # Defined for POST and GET. 
 # GET returns and displays messages filtered by sender,receiver and then ordered by the creation date.
 # POST checks if the form is valid and if so creates the new message and updates the site to include it.
-def message_view(request, username):
-    if request.user.username == username: 
-        return redirect('home')
-    form = MessageForm()
-    if request.method == "POST":
+
+class MessageView(MessageObjectMixin,TemplateView): 
+
+    template_name = 'messaging/message.html' 
+
+    def get(self, request, username):
+        if request.user.username == username: 
+            return redirect('home')
+        form = MessageForm()
+        messages = self.filter_messages(request.user.username, username)
+        context = {
+            'me':request.user.username, 
+            'chatfriend': username, 
+            'messages': messages, 
+            'form': form
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request, username):
+        if request.user.username == username: 
+            return redirect('home')
         form = MessageForm(request.POST or None)
         if form.is_valid():
             instance  = form.save(commit=False)
             instance.Sender = request.user
             instance.Receiver = User.objects.get(username=username)
             instance.save()
-            form = MessageForm()
-    messages = Message.objects.filter(Q(Sender=request.user.username, Receiver= username) | Q(Sender=username, Receiver = request.user.username)).order_by('Date')
-    return render(request, 'messaging/message.html', {'me':request.user.username, 'chatfriend': username, 'messages': messages, 'form': form})
+        form = MessageForm()
+        messages = self.filter_messages(request.user.username, username)
+        context = {
+            'me':request.user.username, 
+            'chatfriend': username, 
+            'messages': messages, 
+            'form': form
+        }
+        return render(request, self.template_name, context)
